7ef4ef6ec1ad71bcc00d1ea910c7b7d93475f843
softacoder <113185323+softacoder@users.noreply.github.com>
2024-01-17 09:46:31 +0000

    commit types definition libraries

---
+++ dist/contentScript.js
@@ -0,0 +1,13 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!********************************************!*\
+  !*** ./src/contentScript/contentScript.ts ***!
+  \********************************************/
+chrome.runtime.sendMessage("From the content script"),
+    (response) => {
+        console.log(response);
+    };
+
+/******/ })()
+;
+//# sourceMappingURL=contentScript.js.map
